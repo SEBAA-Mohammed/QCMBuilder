16da144debff8b737210e51f318b1680c9a613db
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2025-01-03 23:02:01 +0100

    test

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
-
+import 'landingPage' from 'landingPage.tsx'
 function App() {
 
   return (
     <>
+      <landingPage/>
       <div>
         <h1 className="text-3xl font-bold underline">
           Hello world!
