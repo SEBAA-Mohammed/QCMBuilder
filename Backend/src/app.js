8cb8f1a6c76bb26417ba376a6695643a7d538841
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2025-01-06 22:44:56 +0100

    feat : backend : add techer route to app

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const authRoutes = require('./routes/authRoutes');
+const teacherRoutes = require('./routes/teacherRoutes');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/teacher', teacherRoutes);
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
